264cdae86dc4b658663541fefd1dc391f78073e9
sevelinCA <ngabosevelin@gmail.com>
2024-12-02 22:02:49 +0200

    add profile setup

---
+++ app/profile-setup/page.tsx
@@ -0,0 +1,31 @@
+"use client"
+import React, { useState } from 'react'
+import ChooseType from './setup-components/ChooseType'
+import CompanyInfo from './setup-components/CompanyInfo'
+
+const ProfileSetup = () => {
+    const [steps, setSteps] = useState<number>(1)
+    const handelNext = () => {
+        if (steps < 2) {
+            setSteps(steps + 1)
+        }
+    }
+    const handelPrev = () => {
+        if(steps > 1){
+            setSteps(steps - 1)
+        }
+    }
+    return (
+        <div className='min-h-screen flex flex-col bg--white
+         justify-center items-center w-full p-4 gap-[20px]'>
+            {steps == 1 && (<ChooseType />)}
+            {steps == 2 && (<CompanyInfo />)}
+            <div className='flex flex-row mx-auto gap-[10px] items-center'>
+                <button onClick={handelPrev} className='px-10 py-3 rounded-[6px] border border-primary'>Back</button>
+                <button onClick={handelNext} className='px-10 py-3 hover:opacity-80 duration-300 transition-all rounded-[6px] bg-primary text-white'>Next</button>
+            </div>
+        </div>
+    )
+}
+
+export default ProfileSetup
