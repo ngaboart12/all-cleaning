6187479e950479d7b2448600cd018346dd0c809c
sevelinCA <ngabosevelin@gmail.com>
2024-12-02 22:24:54 +0200

    add files step

--- app/profile-setup/page.tsx
+++ app/profile-setup/page.tsx
@@ -2,11 +2,12 @@
 import React, { useState } from 'react'
 import ChooseType from './setup-components/ChooseType'
 import CompanyInfo from './setup-components/CompanyInfo'
+import CompanyDocument from './setup-components/CompanyDocument'
 
 const ProfileSetup = () => {
     const [steps, setSteps] = useState<number>(1)
     const handelNext = () => {
-        if (steps < 2) {
+        if (steps < 3) {
             setSteps(steps + 1)
         }
     }
@@ -20,9 +21,10 @@ const ProfileSetup = () => {
          justify-center items-center w-full p-4 gap-[20px]'>
             {steps == 1 && (<ChooseType />)}
             {steps == 2 && (<CompanyInfo />)}
+            {steps == 3 && (<CompanyDocument />)}
             <div className='flex flex-row mx-auto gap-[10px] items-center'>
                 <button onClick={handelPrev} className='px-10 py-3 rounded-[6px] border border-primary'>Back</button>
-                <button onClick={handelNext} className='px-10 py-3 hover:opacity-80 duration-300 transition-all rounded-[6px] bg-primary text-white'>Next</button>
+                <button onClick={handelNext} className='px-10 py-3 hover:opacity-80 duration-300 transition-all rounded-[6px] bg-primary text-white'>Continue</button>
             </div>
         </div>
     )
--- app/profile-setup/setup-components/CompanyInfo.tsx
+++ app/profile-setup/setup-components/CompanyInfo.tsx
@@ -6,7 +6,7 @@ const CompanyInfo = () => {
         <div className='w-full md:w-1/2 flex flex-col gap-[40px] mx-auto'>
             <div className='flex flex-col gap-[5px]'>
                 <span className='text-[24px] font-[700] text-primary'>Let's get Started</span>
-                <span className='text-[14px] text-[#4e4e4e]'>Fill your company information here</span>
+                <span className='text-[14px] text-[#4e4e4e]'>Add Your Company Bio and Media</span>
 
             </div>
             <form action="" className='flex flex-col gap-[10px]'>
